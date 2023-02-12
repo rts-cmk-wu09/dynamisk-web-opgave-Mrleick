963d2d44444fc1b72cd338fd8112d0ea7051bdf9
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-02-12 19:56:52 +0100

    Added a div to my index.js

--- js/index.js
+++ js/index.js
@@ -3,4 +3,7 @@ let header = document.querySelector(".hero");
 header.innerHTML = `
 <img class="heroimg" src="${hero.image}" alt="Girl in a jacket">
 
+    <div class="herotext">
+     <h1>${hero.headline}</h1>
+    </div>
   `;
