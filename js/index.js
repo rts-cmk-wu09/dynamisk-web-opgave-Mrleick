3050adb4dd3c42d3c7459588b90cae354eb38b41
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-02-13 08:34:14 +0100

    Changed alt text on hero img in index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 let header = document.querySelector(".hero");
 
 header.innerHTML = `
-<img class="heroimg" src="${hero.image}" alt="Girl in a jacket">
+<img class="heroimg" src="${hero.image}" alt="woman in a red shirt">
 
-    <div class="herotext">
-     <h1>${hero.headline}</h1>
+    <div class="herobox">
+     <h1 class="herobox__headline">${hero.headline}</h1>
+     <p class="herobox__copy">${hero.copy}</p>
     </div>
   `;
