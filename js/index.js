c1bb62ffc9152c3de13492d21f47f7dd5b0f5cc2
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-02-13 11:17:00 +0100

    Added code to my service section in index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let header = document.querySelector(".hero");
+let service = document.querySelector(".services")
 
 
 
@@ -12,14 +13,30 @@ for (var i = 0; i < words.length; i++) {
 }
 // Join the words back together into a single string
 var modifiedHeadline = words.join(" ");
+
 header.innerHTML = `
 <img class="heroimg" src="${hero.image}" alt="woman in a red shirt">
 
     <div class="herobox">
      <h1 class="herobox__headline">${modifiedHeadline}</h1>
      <p class="herobox__copy">${hero.copy}</p>
-     <button class="herobox__button">Explore</button>
+     <button class="herobox__button"><img class="herobox__icon" src="${hero.icon}" alt="Imag"><p class="herobox__p">Explore</p></button>
     </div>
   `; 
 
+  let servicesContainer = document.querySelector(".services");
+
+  for (let i = 0; i < services.length; i++) {
+      let service = services[i];
+      let article = document.createElement("article");
+      article.classList.add("servicebox");
+      article.innerHTML = `
+          <img src="${service.illustration}" alt="Illustration for ${service.headline}">
+          <h2 class="servicebox__header">${service.headline}</h2>
+          <p>${service.text}</p>
+          <a href="#">${service.linktext}</a>
+      `;
+      
+      servicesContainer.appendChild(article);
+  }
   
